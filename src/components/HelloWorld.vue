e8aa1bf06ee72425f2cfba9374465f7b41723539
ValerianWeiss <valerian97@web.de>
2020-01-04 17:59:02 +0100

    created temp init state

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,13 @@
 <template>
-<div class="hello-world">
-  <div>
-    <h1 class="center heading">Hello World!</h1>
-    <h4 class="center text">Work in progress{{this.dots}}</h4>
-  </div>
-</div>
+	<div class="hello-world">
+		<div>
+			<h1 class="center heading h1-bright">Hello World!</h1>
+		<div class="center">
+			<h4 class="text h4-bright">Work in progress{{this.dots}}
+			</h4>
+		</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -14,22 +17,30 @@ import '@/main.sass'
 
 @Component({ name: 'hello-world' })
 export default class HelloWorld extends Vue {
-  private dotCount = 0
-  private maxDotCount = 3
-  private dots = ''
+	private dots = '   '
+	private drawDotsFuncId = 0
 
-	constructor() {
-		super()
+	mounted(): void {
 		this.drawDots()
 	}
 
+	destroy(): void {
+		clearInterval(this.drawDotsFuncId)
+	}
 
 	drawDots(): void {
-		setInterval(() => {
-			let result = ''
+		let maxDotCount = 3
 
-			for(let i = 0; i < this.maxDotCount; i++)
-				result += '.'
+		this.drawDotsFuncId = setInterval(() => {
+			let array = this.dots.split('')
+			for (let i = 0; i < array.length; i++) {
+				if (array[i] === ' ') {
+					array[i] = '.'
+					this.dots = array.join('')
+					return
+				}
+			}
+			this.dots = '   '
 		}, 500)
 	}
 }
@@ -37,5 +48,5 @@ export default class HelloWorld extends Vue {
 
 <style lang="sass" scoped>
 .heading
-  margin-top: 40%
+	margin-top: 40vh
 </style>
