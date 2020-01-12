6b36c66a58ca6bf9ed0983562f376a314ac0ef9a
ValerianWeiss <valerian97@web.de>
2020-01-12 17:51:50 +0100

    created buildable state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
-	<div class="home">
-
-	</div>
+  <div class="home">
+    <loading v-if="this.isLoading()"></loading>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import Loading from '@/views/Loading.vue'
 import '@/main.sass'
 
-@Component({ name: 'home' })
+@Component({
+  name: 'home',
+  components: { Loading }
+})
 export default class Home extends Vue {
-
+  isLoading() {
+    return true
+  }
 }
 </script>
 
---
+++ src/views/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="loading">
+
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import '@/main.sass'
+
+@Component({ name: 'loading' })
+export default class Loading extends Vue {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
