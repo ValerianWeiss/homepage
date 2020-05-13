00b7d24ed030ecefa0258468e52010ce86a15bef
Valerian Weiß <valerian97@web.de>
2020-05-13 22:21:26 +0200

    implemented ProtfolioItem fade animation, when Artwork is getting switched

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="portfolio-item">
+  <div class="portfolio-item" :ref="this.portfolioItemRef">
     <div class="heading">
-      <h1>{{this.artwork.title}}</h1>
+      <h1>{{this.activeArtwork.title}}</h1>
     </div>
     <div class="image-container float-left">
       <img
         class="image width-height-100"
-        :src="this.artwork.previewImgUrl"
-        alt="artwork image"
-      >
+        :src="this.activeArtwork.previewImgUrl"
+        alt="artwork image">
     </div>
-    <div class="text-container" v-html="this.descriptionText"></div>
+    <div
+      class="text-container"
+      v-html="this.descriptionText">
+    </div>
   </div>
 </template>
 
@@ -26,16 +28,31 @@ interface DescriptionTextCacheItem {
   descriptionText: string
 }
 
+enum Fading {
+  IN,
+  OUT
+}
+
 @Component({ name: 'protfolio-item' })
 export default class PortfolioItem extends Vue {
   @Prop()
   public artwork: Artwork
 
+  private activeArtwork: Artwork
   private descriptionText: string = ''
   private descriptionTextCache: DescriptionTextCacheItem[] = []
+  private fadeInOutMaxDuration: number = 250
+  private portfolioItemRef: string = 'portfolio-item'
+  private fadeAnimationQueue: Promise<void>[] = []
+
+  public constructor() {
+    super()
+    this.activeArtwork = this.artwork
+  }
 
   private mounted(): void {
     this.updateDescriptionText(this.artwork)
+    this.startFadeArtworkAnimation(Fading.IN)
   }
 
   private updateDescriptionText(artwork: Artwork) {
@@ -64,6 +81,49 @@ export default class PortfolioItem extends Vue {
     })
   }
 
+  private startFadeArtworkAnimation(fading: Fading): Promise<void> {
+    return new Promise<void>(resolve => {
+      if (fading === Fading.IN) this.activeArtwork = this.artwork
+      let portfolioItem = this.getPortfolioItem()
+      let startTime = new Date().getTime()
+
+      let animationInterval = setInterval(() => {
+        let ellapsedTime = new Date().getTime() - startTime
+
+        if (ellapsedTime >= this.fadeInOutMaxDuration) {
+          portfolioItem.style.opacity = fading === Fading.IN ? '1' : '0'
+          clearInterval(animationInterval)
+          resolve()
+        }
+
+        let progress = ellapsedTime / this.fadeInOutMaxDuration
+        let opacity = fading === Fading.IN ? progress : 1 - progress
+        portfolioItem.style.opacity = `${opacity}`
+      }, 1000 / 60)
+    })
+  }
+
+  private fadeArtwork(): Promise<void> {
+    let animationPromise = new Promise<void>(resolve => {
+      Promise.all(this.fadeAnimationQueue).then(() => {
+        this.startFadeArtworkAnimation(Fading.OUT).then(() => {
+          this.startFadeArtworkAnimation(Fading.IN).then(() => {
+            resolve()
+            let animationIndex = this.fadeAnimationQueue.indexOf(animationPromise)
+            this.fadeAnimationQueue.splice(animationIndex, 1)
+          })
+        })
+      })
+    })
+
+    this.fadeAnimationQueue.push(animationPromise)
+    return animationPromise
+  }
+
+  private getPortfolioItem(): any {
+    return this.$refs[this.portfolioItemRef]
+  }
+
   private insertDescriptionText(descriptionText: string): void {
     this.descriptionText = descriptionText
   }
@@ -71,6 +131,7 @@ export default class PortfolioItem extends Vue {
   @Watch('artwork')
   private onArtworkChange(newArtwork: Artwork, oldArtwork: Artwork): void {
     this.updateDescriptionText(newArtwork)
+    this.fadeArtwork()
   }
 }
 </script>
@@ -78,6 +139,7 @@ export default class PortfolioItem extends Vue {
 <style lang="sass" scoped>
 .portfolio-item
   height: 100%
+  opacity: 0
 
 .heading
   height: 80px
@@ -95,7 +157,7 @@ export default class PortfolioItem extends Vue {
   height: calc(100% - 110px)
   width: 60%
   float: left
-  overflow-y: auto
+  overflow: auto auto
 
 .text
   margin: 0 25px 15px 25px
